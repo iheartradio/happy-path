98400d9e0ff266ceb2ed56dbba418c077f50eade
Kailuo Wang <kailuo.wang@gmail.com>
2016-05-17 11:08:58 -0400

    minor publish setting change

--- project/Publish.scala
+++ project/Publish.scala
@@ -5,17 +5,17 @@ import bintray.BintrayKeys._
 object Publish {
 
   val bintraySettings = Seq(
-    bintrayOrganization := Some("kailuowang"),
-    bintrayPackageLabels := Seq("henkan")
+    bintrayOrganization := Some("iheartradio"),
+    bintrayPackageLabels := Seq("Future", "Either", "Functional", "Xor", "Scala")
   )
 
   val publishingSettings = Seq(
-    organization in ThisBuild := "com.kailuowang",
+    organization in ThisBuild := "com.iheart",
     publishMavenStyle := true,
     licenses := Seq("Apache-2.0" -> url("https://www.apache.org/licenses/LICENSE-2.0.html")),
-    homepage := Some(url("http://kailuowang.github.io/henkan")),
-    scmInfo := Some(ScmInfo(url("https://github.com/kailuowang/henkan"),
-      "git@github.com:kailuowang/henkan.git")),
+    homepage := Some(url("http://iheartradio.github.io/happy-path")),
+    scmInfo := Some(ScmInfo(url("https://github.com/iheartradio/happy-path"),
+      "git@github.com:iheartradio/happy-path.git")),
     pomIncludeRepository := { _ => false },
     publishArtifact in Test := false
   )
